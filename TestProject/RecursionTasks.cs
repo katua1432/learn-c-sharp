bb984d9672e4d0b90e5da06bb94ba7bf50827b60
Katsiaryna Semianiuk <k.semianiuk@godeltech.com>
2024-05-17 18:59:31 +0200

    recusion tasks

---
+++ TestProject/RecursionTasks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class RecursionTasks
+    {
+        public void Run() 
+        {
+            //task 1
+
+            static string RecursionIsPalindrome(string wordToCheck)
+            {
+
+                if (wordToCheck.Length < 2) //дошли до середины
+                {
+                    return "YES";
+                }
+                else if (wordToCheck[0] != wordToCheck[wordToCheck.Length - 1]) // видим, что не совпадает
+                {
+                    return "NO";
+                }
+                //чек крайних символов
+
+                //продолжаем двигаться минус крайние символы
+
+                return RecursionIsPalindrome(wordToCheck.Substring(1, wordToCheck.Length - 2));
+            }
+            //Console.WriteLine(RecursionIsPalindrome("kayak"));
+
+
+            //task 2
+
+            static string RecursionNumbersOutput(int numberToRecurse, string output = "")
+            {
+
+                output += $"{numberToRecurse}";
+
+                if (numberToRecurse == 1)
+                {
+                    return output;
+                }
+                else
+                {
+                    var result = output + RecursionNumbersOutput(--numberToRecurse);
+
+                    return result;
+                }
+            }
+
+            //Console.WriteLine(RecursionNumbersOutput(10));
+        }
+    }
+}
